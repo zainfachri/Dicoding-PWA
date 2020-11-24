5e5383d87397f24af98dc04f7b0fed426c6f9429
M. Fachri Zain <zainfachri23@gmail.com>
2020-11-24 16:46:48 +0700

    update service worker

--- service-worker.js
+++ service-worker.js
@@ -89,7 +89,9 @@ workbox.precaching.precacheAndRoute([
   { url: 'https://unpkg.com/snarkdown@1.0.2/dist/snarkdown.umd.js', revision: '1' },
   { url: 'https://fonts.googleapis.com/icon?family=Material+Icons', revision: '1' },
   { url: 'https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2', revision: '1' },
-]);
+], {
+  ignoreUrlParametersMatching: [/.*/]
+});
 
 //=================================================//
 //=================================================//
